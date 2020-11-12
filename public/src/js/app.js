02725eb2e615d989594daf54869d1aec66d2ea53
rumeysayilmaz <era.ylmaz@gmail.com>
2020-11-12 15:52:49 +0300

    On branch master [REV] modified the following for Marmara Explorer  Changes to be committed: 	modified:   public/img/icons/favicon.ico 	modified:   public/img/logo.png 	modified:   public/js/main.min.js 	modified:   public/src/js/app.js 	modified:   public/views/includes/connection.html 	modified:   public/views/includes/header.html 	modified:   public/views/index.html 	modified:   public/views/messages_verify.html 	modified:   public/views/status.html 	modified:   public/views/transaction_sendraw.html

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var testnet = false;
-var netSymbol = testnet ? 'TAZ' : 'KMD';
+var netSymbol = testnet ? 'TAZ' : 'MCL';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
 var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
