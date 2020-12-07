c5040e19589cd7906221b109443646eba77de6bb
Rumeysa Yilmaz <44261315+rumeysayilmaz@users.noreply.github.com>
2020-12-07 17:53:46 +0300

    Update main.js
    [imp] added marmara related texts

--- public/js/main.js
+++ public/js/main.js
@@ -1571,7 +1571,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash/', {
       templateUrl: 'views/block.html',
-      title: 'Komodo Block '
+      title: 'Marmara Block '
     }).
     when('/block-index/:blockHeight/', {
       controller: 'BlocksController',
@@ -1583,7 +1583,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?/', {
       templateUrl: 'views/transaction.html',
-      title: 'Komodo Transaction '
+      title: 'Marmara Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -1595,15 +1595,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks/', {
       templateUrl: 'views/block_list.html',
-      title: 'Komodo Blocks solved Today'
+      title: 'Marmara Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?/', {
       templateUrl: 'views/block_list.html',
-      title: 'Komodo Blocks solved '
+      title: 'Marmara Blocks solved '
     }).
     when('/address/:addrStr/', {
       templateUrl: 'views/address.html',
-      title: 'Komodo Address '
+      title: 'Marmara Address '
     }).
     when('/charts/:chartType?/', {
       templateUrl: 'views/charts.html',
